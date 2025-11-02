c6cd3362987c35c9c84a2beafab45fa161d215c6
KenzoTzy90 <gusionzainal@gmail.com>
2025-11-02 13:26:59 +0700

    Feat: Upload Tugas 3

---
+++ TUGAS 3/index.php
@@ -0,0 +1,26 @@
+<?php
+class LuasLingkaran {
+    public const phi = 3.14;
+    public int $jari;
+
+    public function __construct($isijari = 1) {
+        $this->jari = $isijari;
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+    
+    public static function testing() {
+        echo "ini dari static";
+    }
+
+    public function __destruct() {
+        echo "udah ah capekkkk";
+    }
+}
+$Lingkaran = new LuasLingkaran(5);
+$Lingkaran->tampil('roda');
+LuasLingkaran::testing();
+?>
