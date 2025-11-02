0e34ad4c849705898caa7fa90a46f62e3735f18d
KenzoTzy90 <gusionzainal@gmail.com>
2025-11-02 13:55:28 +0700

    Feat: Upload Tugas 6

---
+++ TUGAS 6/LuasLingkaran.php
@@ -0,0 +1,27 @@
+<?php
+class LuasLingkaran {
+    public const phi = 3.14;
+    public int $jari;
+
+    public function __construct($isijari = 1) {
+            $this->jari = $isijari;
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+        }
+
+        public static function testing() {
+            echo "ini dari static";
+        }
+
+        public function __destruct() {
+            echo "udah ah capek";
+        }
+}
+
+$Lingkaran = new LuasLingkaran(5);
+$Lingkaran->tampil('roda');
+LuasLingkaran::testing();
+?>
---
+++ TUGAS 6/index.php
@@ -0,0 +1,28 @@
+<?php
+class LuasLingkaran {
+    public const phi = 3.14;
+    private int $jari;
+
+    public function __construct($isijari = 1) {
+            $this->jari = $isijari;
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+        }
+
+        public static function testing() {
+            echo "ini dari static";
+        }
+
+        public function __destruct() {
+            echo "udah ah capek";
+        }
+}
+
+$Lingkaran = new LuasLingkaran(5);
+// $Lingkaran->jari = 5;
+$Lingkaran->tampil('roda');
+LuasLingkaran::testing();
+?>
